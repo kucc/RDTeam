f24abaf14368c578190c7bae46f09d1f3bffa2f9
choihamin <jason4284@korea.ac.kr>
2020-11-07 07:39:23 +0900

    [backend] merged

--- backend/realdragongame/game/services/vote_action.py
+++ backend/realdragongame/game/services/vote_action.py
@@ -2,7 +2,7 @@ import uuid
 
 from rest_framework.exceptions import NotFound, APIException
 
-from game.models import Room, User, Vote, Game
+from game.models import Room, User, Vote, Game, UserGameState, Role
 from game.services.fetch_room import calculate_total_vote_count
 
 
@@ -21,15 +21,47 @@ def vote_action(room_code, user_id, target_id):
     game = Game.objects.filter(room=room).last()
     votes = Vote.objects.filter(round=game.round, game=game)
 
-    if Vote.objects.count() == 0:
-        vote = Vote(target_user__unique_id=target_id, user__unique_id=user_id, round=game.round, game=game)
-        vote.save()
+    aliver = UserGameState.objects.filter(game=game, is_alive=True)
 
-    for vote in votes:
-        if vote.user.unique_id != user_id:
-            vote = Vote(target_user__unique_id=target_id, user__unique_id=user_id, round=game.round, game=game)
-            vote.save()
-        else:
+    aliver_id_set = set(map(lambda u: u.unique_id, aliver))
+    if not(user_id in aliver_id_set):
+        raise APIException(code=400)
+
+    for v in votes:
+        if v.user.unique_id == user_id:
             raise APIException(code=400)
+
+    vote = Vote(target_user=target, user=user, round=game.round, game=game)
+    vote.save()
+
+    vote_set = set(map(lambda u: u.unique_id, votes))
+
+
+
+
+
+    if len(vote_set) == len(aliver_id_set):
+        result = calculate_total_vote_count(votes)
+
+        lst = list(result.keys())
+        for e in lst:
+            if result[e] == max(result.values()):
+                to_be_killed = next(filter(lambda u: u.unique_id == e, aliver))
+                to_be_killed.is_alive = False
+
+                if Role.objects.filter(game=game, user=to_be_killed.user).first().role_name == Role.MAFIA:
+                    pass
+                    #시민승리
+
+
+        game.state = Game.GUSSING
+
+
+
+
+
+    if len(aliver_id_set) == 2:
+        game.state = Game.END
+        #마피아 승리
 
     return VoteResult(room, user, target)
