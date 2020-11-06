959c9a018eedf1e0e20c726bed8d53fea6aadfbb
ffaass <rlatnghd95@gmail.com>
2020-11-07 04:16:21 +0900

    [backend] heroku deploy settings

--- backend/realdragongame/config/settings.py
+++ backend/realdragongame/config/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'rest_framework'
 ]
 
@@ -120,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
