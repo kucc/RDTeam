c1f481d7533f4e479f5ce2c9e403424d73d6b118
choihamin <jason4284@korea.ac.kr>
2020-11-07 07:39:28 +0900

    [backend] merged

--- backend/realdragongame/game/services/make_description.py
+++ backend/realdragongame/game/services/make_description.py
@@ -15,21 +15,29 @@ def make_description(room_code, user_id, description):
         raise APIException(code=400)
 
     user = User.objects.filter(unique_id=user_id).first()
+
     round_subject = RoundSubject.objects.filter(
         game=game,
         round=game.round).first()
+
+
     RoundSubjectDescription(round_subject=round_subject, user=user, description=description).save()
 
     round_subject_descriptions = RoundSubjectDescription.objects.filter(round_subject=round_subject)
+
+
     user_id_set = set()
     for subject_description in round_subject_descriptions:
         user_id_set.add(subject_description.user.id)
-
     describable_users = list(filter(lambda u: u.id not in user_id_set, room.user_set.all()))
+
+
     describable_users = sorted(describable_users, key=lambda u: u.id)
     if len(describable_users) == 0:
         game.state = Game.VOTING
     else:
         game.current_describer = describable_users[0]
+
+
     game.save()
 
